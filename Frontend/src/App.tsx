3d38971046c15a9372cc64587e76b93d5704433b
Christine Kim <kim2christine@gmail.com>
2024-05-07 09:13:54 -0400

    added addReimbursement

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { Homepage } from "./components/Login/Homepage";
 import { Catch } from "./components/Catch/Catch";
 import { Register } from "./components/Login/Register";
 import { Collection } from "./components/Collection/Collection";
+import { AddReimbursement } from "./components/Login/AddReimbursement";
 //import all of the other components here!!!!!!!!
 //still have to make other components for reimbursement retrieval/status changes
 
@@ -19,7 +20,8 @@ function App() {
         <Routes>
           <Route path="" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/reimbursement/add" element={<Catch />} />
+          <Route path="/reimbursement/add" element={<AddReimbursement />} />
+          <Route path="/reimbursement/view" element={<Catch />} />
           <Route path="/homepage" element={<Homepage />} />
           <Route path="/reimbursement" element={<Collection />} />
         </Routes>
---
+++ Frontend/src/components/Login/AddReimbursement.tsx
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { ReimbursementInterface } from "../Interfaces/ReimbursementInterface";
+
+import "./Add.css";
+
+export const AddReimbursement: React.FC = () => {
+  //set state (UserInterface)
+  const [reimbursement, setReimbursement] = useState<ReimbursementInterface>({
+    description: "",
+    amount: 0,
+    status: "",
+  });
+
+  //useNavigate to navigate between components
+  const navigate = useNavigate();
+
+  //function to store input box values
+  const storeValues = (input: any) => {
+    //if the input that has changed is the "username" input, change the value of username in the user state object
+
+    if (input.target.name === "description") {
+      setReimbursement((reimbursement) => ({
+        ...reimbursement,
+        description: input.target.value,
+      }));
+    } else {
+      setReimbursement((reimbursement) => ({
+        ...reimbursement,
+        amount: input.target.value,
+      }));
+    }
+    console.log(reimbursement);
+  };
+
+  //function to send a POST with user data to register a user in the backend
+  //! Remember, requests to our Java server will only work with @CrossOrigin in our Controllers
+  const add = async () => {
+    //this will go to the backend
+    const response = await axios.post(
+      "http://localhost:8080/reimbursement/add",
+      reimbursement,
+      { withCredentials: true }
+    );
+    console.log(response);
+    navigate("/reimbursement/view");
+  };
+
+  return (
+    <div className="add">
+      <div className="text-container">
+        <h1>Submit a new Reimbursement Form!</h1>
+
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="description"
+            name="description"
+            onChange={storeValues}
+          />
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="amount"
+            name="amount"
+            onChange={storeValues}
+          />
+        </div>
+
+        <button className="add-reimbursement" onClick={add}>
+          Submit
+        </button>
+        <button
+          className="add-reimbursement"
+          onClick={() => navigate("/homepage")}
+        >
+          Back
+        </button>
+      </div>
+    </div>
+  );
+};
